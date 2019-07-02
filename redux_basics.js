ba9983b9ac0476bb77742ebaf3f8166cb529d418
Hardev Sharma <hd.sharma86@gmail.com>
2019-07-02 23:45:50 +0530

    Initial Commit

---
+++ redux_basics.js
@@ -0,0 +1,39 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const initialState = {
+    counter: 0
+};
+
+// Reducers
+const rootReducer = (state = initialState, action) => {
+
+    if(action.type === 'INC_COUNTER'){
+        return {
+            ...state,
+            counter: state.counter + 1
+        };
+    }
+
+    if(action.type === 'ADD_COUNTER'){
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
+
+    return state;
+    
+}
+
+// Store
+const store = createStore(rootReducer);
+
+// Subscription
+store.subscribe(() => {
+    console.log('[Subscription]: ',store.getState());
+});
+
+// Dispatching actions...
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
